f51f8eeb7ac859190971af68a0e0e023691fd512
TheMellowSheep <melodiekouy@gmail.com>
2022-03-20 12:07:28 +0100

    add the possibility to manage the character's movements

--- src/game-script.js
+++ src/game-script.js
@@ -1,6 +1,8 @@
 /* Game script ! :D */
+"use strict" // allow us to do less errors
 
 var timeSet;
+window.addEventListener('keydown', move); // for the character's movements
 
 function onload() {
 	/* the canvas will be displayed after 1s */
@@ -11,4 +13,35 @@ function onload() {
 function showPage() {
 	document.getElementById("circleLoader").style.display = "none";
 	document.getElementById("canvas").style.display = "block"
+}
+
+/* manage the character's movements*/
+function move (event){
+	const e = event.keyCode;
+	switch (e) {
+		// left, q
+		case 37 : case 81 :
+			console.log(`left`);
+			break;
+
+		// up, z
+		case 38 : case 90 :
+			console.log(`up`);
+			break;
+
+		// right, d
+		case 39 : case 68 : 	
+			console.log(`right`);
+			break;
+
+		// down, s
+		case 40 : case 83 : 	
+			console.log(`down`);
+			break;
+		
+		// ignore, do nothing
+		default:
+			console.log(`ignore this key (ASCII code : ${e})`);
+			break;
+	}
 }
